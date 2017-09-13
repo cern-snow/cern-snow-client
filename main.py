3c46d6d1c90e44930551231bb152bedcd6a21173
James Clerc <clercj@lxplus046.cern.ch>
2017-09-13 09:57:22 +0200

    main is complete the test main is fine there is example in the main

--- main.py
+++ main.py
@@ -4,12 +4,28 @@ import json
 def main():
         s = sn.SnowRestSession()
         s.loadConfigFile('config.yaml')
-	#b = s.getIncident(number='INC1441784')
-	#print b.text
-	#c = s.post(s.instance + '/oauth_token.do', None, data = {'grant_type' : 'password', 'client_id' : s.oauthClientId, 'client_secret' : s.oauthClientSecret})
-	#print c.text
-	a = s.addWorkNote(table='incident', number='INC1422216', workNote='ON EST LA')
-	print a.text
+	#result = s.get(url='', params={})
+	#result = s.posturl='', headers={}, data={})
+	#result = s.put(url='', headers={}, data={})
+	#result = s.getRecord(table='', id='', number='')
+	#result = s.getRecords(table='', filter={}, encodedQuery='')
+	#result = s.getIncidents(filter={}, encodedQuery='')
+	#result = s.getIncident(id='', number='')
+	#result = s.insertRecord(table='', data={})
+	#result = s.insertIncident(data={})
+	#result = s.updateRecord('incident', id='12345feab...')                             
+	#result = s.updateRecord('incident', number='INC12345')
+	#result = s.updateIncident(id='12345feab...', data=data)
+	#result = s.updateIncident(number='1234561', data=data)
+	#result = s.incAddComment(id='', comment='')
+        #result = s.incAddComment(number='', comment='')
+	#result = s.addComment(table='', id='', comment='')
+	#result = s.addComment(table='', number='', comment='')
+	#result = s.incAddWorkNote(id='', workNote='')
+	#result = s.incAddWorkNote(number='', workNote='')
+	#result = s.addWorkNote(table='', id='', workNote='')
+	#result = s.addWorkNote(table='', number='', workNote='')
+	print result.text
 	
 
 if __name__ == '__main__':
