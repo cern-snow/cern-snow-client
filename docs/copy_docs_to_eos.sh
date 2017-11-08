dfd545e3c197b8a84ed3cc19c3a0c72c4fc6b6ab
David Martin Clavo <david.martin.clavo@cern.ch>
2017-11-08 18:15:30 +0100

    Fix in script to copy docs to eos

--- docs/copy_docs_to_eos.sh
+++ docs/copy_docs_to_eos.sh
@@ -3,4 +3,4 @@
 # This file should be run from the current directory, i.e.
 # ./copy_docs_to_eos.sh
 
-rsync -av --delete docs/_build/html/ /eos/project/s/servicenow/www/snow-client-docs
+rsync -av --delete _build/html/ /eos/project/s/servicenow/www/snow-client-docs
