531fb4349632fb29fa7cd144c74aac7d9473761a
David Martin Clavo <david.martin.clavo@cern.ch>
2017-11-09 16:15:35 +0100

    Updated script to run all tests

--- tests/run.sh
+++ tests/run.sh
@@ -6,5 +6,7 @@
 # ./run.sh
 
 # Run the tests the Python 2.6 way, to make sure they can run in lxplus.cern.ch (2.6) and aiadm.cern.ch (2.7)
-python -m tests.basic_auth_test
-python -m tests.sso_oauth_test
+python -m tests.test_session_basic_auth
+python -m tests.test_session_sso_oauth
+python -m tests.test_record_basic_auth
+python -m tests.test_record_sso_oauth
