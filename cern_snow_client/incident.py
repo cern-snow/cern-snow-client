5ecd1c2b98740e445a119257053952a229af6c0b
David Martin Clavo <david.martin.clavo@cern.ch>
2017-11-09 17:26:11 +0100

    Start of documentation for Task and Incident classes

--- cern_snow_client/incident.py
+++ cern_snow_client/incident.py
@@ -8,8 +8,40 @@ package_table = 'incident'
 
 
 class Incident(Task):
+    """
+        The class Task has all the methods of the Record and Task classes,
+        plus some extra methods.
 
-    def __init__(self, session, values={}):
+        Args:
+            session (SnowRestSession): a cern_snow_client.session.SnowRestSession object, which will be used
+                to authenticate and communicate with ServiceNow
+            values (:obj:`dict`, optional): initial values for the record
+
+        Examples:
+
+            Inserting a new incident:
+
+            >>> inc = Incident(s)  # s is a SnowRestSession object
+            >>> inc.short_description = "New incident"
+            >>> inc.u_business_service = 'e85a3f3b0a0a8c0a006a2912f2f352d1'  # Service Element "ServiceNow"
+            >>> inc.u_functional_element = '579fb3d90a0a8c08017ac8a1137c8ee6'  # Functional ELement "ServiceNow"
+            >>> inc.comments = "Initial description"
+            >>> inc.incident_state = '2'   # request state = Assigned
+            >>> inc.insert()
+
+            Adding a comment to an incident after getting it:
+
+            >>> inc = Incident(s)  # s is a SnowRestSession object
+            >>> if inc.get('c1c535ba85f45540adf94de5b835cd43'):
+            >>>     inc.add_comment('New comment')  # will immediately add a new comment in ServiceNow
+
+            Resolving an Incident for which we know the number:
+
+            >>> inc = Incident(s)  # s is a SnowRestSession object
+            >>> inc.resolve('New comment', close_code='Not Reproducible', key=('number', 'INC0426232'))
+        """
+
+    def __init__(self, session, values=None):
         Record.__init__(self, session, package_table, values=values)
 
 
--- cern_snow_client/task.py
+++ cern_snow_client/task.py
@@ -7,20 +7,129 @@ package_table = 'task'
 
 
 class Task(Record):
+    """
+    The class Task has all the methods of the Record class,
+    plus some extra methods.
 
-    def __init__(self, session, values={}):
-        Record.__init__(self, session, package_table, values=values)
+    It is not possible to insert a Task, unless you specify a table_name value with a subtable.
+    Alternatively, you should instantiate a subclass, such as Incident.
 
-    def add_comment(self, comment):
+    Args:
+        session (SnowRestSession): a cern_snow_client.session.SnowRestSession object, which will be used
+            to authenticate and communicate with ServiceNow
+        table_name (:obj:`str`, optional): the name of a ServiceNow table,
+            e.g. 'incident', for this task
+        values (:obj:`dict`, optional): initial values for the record
+
+    Examples:
+        >>> t = Task(s)  # s is a SnowRestSession object
+        >>> if t.get('c1c535ba85f45540adf94de5b835cd43'):
+        >>>     print t.number  # 'INC0426232'
+        >>>     print t.sys_class_name  # 'incident'
+        >>>     t.watch_list = 'new_value@cern.ch'
+        >>>     t.update()   # updates the watch list
+        >>>     t.add_comment('New comment')  # will immediately add a new comment in ServiceNow
+        >>>     t.add_work_note('New work note')  # will immediately add a new work note in ServiceNow
+        >>>     t.resolve('Solution text')  # will immediately resolve the incident: Incident State=Resolved,
+        >>>                                 # Close Code=Restored, and a new comment/solution will be added
+    """
+
+    def __init__(self, session, table_name=package_table, values=None):
+        Record.__init__(self, session, table_name, values=values)
+
+    def add_comment(self, comment, key=None):
+        """
+        Adds a comment in this Task in ServiceNow.
+        The comment will be sent immediately, without a need to call .update()
+
+        Args:
+            comment (str): the new comment to be added
+            key (:obj:`str` or :obj:`tuple`, optional): a str the sys_id (ServiceNow unique identifier)
+                of the record to update, or a tuple (field_name, field_value), such as ('number', 'INC987654').
+                If this parameter is set, it will override the current sys_id of this record object, if any.
+
+        Returns:
+            bool: True if the record was updated succesfully, False otherwise.
+
+        Raises:
+            SnowClientException: if no key is provided and the current record has no ``sys_id``
+            SnowRestSessionException : if there is any authentication problem
+
+        Examples:
+
+            Adding a comment to a task after getting it:
+
+            >>> t = Task(s)  # s is a SnowRestSession object
+            >>> if t.get('c1c535ba85f45540adf94de5b835cd43'):
+            >>>     t.add_comment('New comment')  # will immediately add a new comment in ServiceNow
+
+            Adding a comment to a task for which we know the number:
+
+            >>> t = Task(s)  # s is a SnowRestSession object
+            >>> t.add_comment('New comment', key=('number', 'INC0426232'))  # will immediately add a new comment in ServiceNow, and will download the resulting state of the task
+        """
         # TODO: Finish this method
         pass
 
-    def add_work_note(self, work_note):
+    def add_work_note(self, work_note, key=None):
+        """
+        Similar to add_comment, but to add work notes.
+        The work note will be sent immediately, without a need to call .update()
+
+        Args:
+            work_note (str): the new comment to be added
+            key (:obj:`str` or :obj:`tuple`, optional): a str the sys_id (ServiceNow unique identifier)
+                of the record to update, or a tuple (field_name, field_value), such as ('number', 'INC987654').
+                If this parameter is set, it will override the current sys_id of this record object, if any.
+
+        Returns:
+            bool: True if the record was updated succesfully, False otherwise.
+
+        Raises:
+            SnowClientException: if no key is provided and the current record has no ``sys_id``
+            SnowRestSessionException : if there is any authentication problem
+        """
         # TODO: Finish this method
         pass
 
-    def resolve(self, comment, close_code):
+    def resolve(self, solution, close_code=None, key=None):
+        """
+        Resolves a Task in ServiceNow.
+        The Task will be resolved immediately, without a need to call .update()
+
+        Args:
+            solution (str): the text that will be added to the comments and which will be stored as the Solution
+                of this Task
+            close_code (:obj:`str`, optional): the Close Code to use. If not set, the default Close Code will be
+                selected : "Restored" for Incidents and "Fulfilled" for Requests.
+            key (:obj:`str` or :obj:`tuple`, optional): a str the sys_id (ServiceNow unique identifier)
+                of the record to update, or a tuple (field_name, field_value), such as ('number', 'INC987654').
+                If this parameter is set, it will override the current sys_id of this record object, if any.
+
+        Returns:
+            bool: True if the record was updated succesfully, False otherwise.
+
+        Raises:
+            SnowClientException: if no key is provided and the current record has no ``sys_id``
+            SnowRestSessionException : if there is any authentication problem
+
+        Examples:
+            Resolving a Task after getting it:
+
+            >>> t = Task(s)  # s is a SnowRestSession object
+            >>> if t.get('c1c535ba85f45540adf94de5b835cd43'):
+            >>>     t.resolve('Solution text')  # will immediately resolve in ServiceNow
+
+            Resolving a Task for which we know the number, with close code 'Rejected':
+
+            >>> t = Task(s)  # s is a SnowRestSession object
+            >>> t.resolve('New comment', close_code='Works as designed', key=('number', 'INC0426232'))
+        """
+
         # TODO: Finish this method
+        # if an incident, make : incident_state=6, comments=solution, u_close_code=close_code
+        # if a request, make : u_current_task_state=9, comments=solution, u_close_code=close_code
+        # if no close_code is provided, choose 'Restored' for incident and 'Fulfilled' for request
         pass
 
 
