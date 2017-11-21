fe695577ad4435d763484450bf814fe649027570
David Martin Clavo <david.martin.clavo@cern.ch>
2017-11-21 18:21:53 +0100

    Add new lines to echo in tests/run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -6,23 +6,23 @@
 # ./run.sh
 
 # Run the tests the Python 2.6 way, to make sure they can run in lxplus.cern.ch (2.6) and aiadm.cern.ch (2.7)
-echo "python -m tests.test_session_basic_auth"
+echo -e "\n\npython -m tests.test_session_basic_auth"
 python -m tests.test_session_basic_auth
 
-echo "python -m tests.test_session_sso_oauth"
+echo -e "\n\npython -m tests.test_session_sso_oauth"
 python -m tests.test_session_sso_oauth
 
-echo "python -m tests.test_record_basic_auth"
+echo -e "\n\npython -m tests.test_record_basic_auth"
 python -m tests.test_record_basic_auth
 
-echo "python -m tests.test_record_sso_oauth"
+echo -e "\n\npython -m tests.test_record_sso_oauth"
 python -m tests.test_record_sso_oauth
 
-echo "python -m tests.test_record_field"
+echo -e "\n\npython -m tests.test_record_field"
 python -m tests.test_record_field
 
-echo "python -m tests.test_task_basic_auth"
+echo -e "\n\npython -m tests.test_task_basic_auth"
 python -m tests.test_task_basic_auth
 
-echo "python -m tests.test_task_sso_oauth"
+echo -e "\n\npython -m tests.test_task_sso_oauth"
 python -m tests.test_task_sso_oauth
