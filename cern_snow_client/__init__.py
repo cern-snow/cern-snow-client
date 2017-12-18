ebae3ff6d1d92e41042bb70841f38f86f863eae9
David Martin Clavo <david.martin.clavo@cern.ch>
2017-12-18 15:38:44 +0100

    Mentioning the license in library source code files. Version bump to 0.3.0.1 Changelog

--- cern_snow_client/__init__.py
+++ cern_snow_client/__init__.py
@@ -1,4 +1,23 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
+#
+# This file is part of the cern-snow-client library.
+# Copyright (c) 2017 CERN
+# Authors: James Clerc <james.clerc@cern.ch>, David Martin Clavo <david.martin.clavo@cern.ch>
+#
+# The cern-snow-client library is free software; you can redistribute it
+# and/or modify it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# The cern-snow-client library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with the cern-snow-client library.  If not, see <http://www.gnu.org/licenses/>.
+#
+# In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
 
 """
 The CERN ServiceNow Client library (CERN SNow Client) facilitates the usage of the ServiceNow REST API
@@ -9,7 +28,7 @@ Also, other higher level operations have been implemented, such as querying/inse
 or querying/inserting/updating incidents and requests, including adding comments and work notes.
 """
 
-__version__ = "0.3"
+__version__ = "0.3.0.1"
 
 
 import logging
