50855513490faa563e349a1b344c065147a3505a
David Martin Clavo <david.martin.clavo@cern.ch>
2017-12-18 15:39:03 +0100

    Mentioning the license in tests and example Python files

--- tests/test_session_base.py
+++ tests/test_session_base.py
@@ -1,4 +1,22 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
+#
+# This file is part of the cern-snow-client library.
+# Copyright (c) 2017 CERN
+# Authors: James Clerc <james.clerc@cern.ch>, David Martin Clavo <david.martin.clavo@cern.ch>
+#
+# The cern-snow-client library is free software; you can redistribute it
+# and/or modify it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# The cern-snow-client library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with the cern-snow-client library.  If not, see <http://www.gnu.org/licenses/>.
+#
+# In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
 
 import json
 
